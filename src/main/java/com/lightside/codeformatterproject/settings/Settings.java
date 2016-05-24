549dabfe801eabb79e00f669a46861af7b336d68
Svetlana Kuzina <lost.life.omsk@gmail.com>
2016-05-24 17:32:28 +0600

    Fixing bugs

--- src/main/java/com/lightside/codeformatterproject/settings/Settings.java
+++ src/main/java/com/lightside/codeformatterproject/settings/Settings.java
@@ -1,8 +1,5 @@
 package com.lightside.codeformatterproject.settings;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -20,7 +17,8 @@ public class Settings {
      */
     public Settings(final Properties props) {
         this.lineBreak = props.getProperty("line_break");
-        this.offsetLength = Integer.valueOf(props.getProperty("offset_length"));
+        String temp = props.getProperty("offset_length");
+        this.offsetLength = Integer.valueOf(temp);
         this.offsetSymbol = props.getProperty("offset_symbol").charAt(0);
     }
 
@@ -29,23 +27,12 @@ public class Settings {
         return offsetSymbol;
     }
 
-    public void setOffsetSymbol(final char offsetSymbol) {
-        this.offsetSymbol = offsetSymbol;
-    }
-
     public String getLineBreak() {
         return lineBreak;
-    }
-
-    public void setLineBreak(final String lineBreak) {
-        this.lineBreak = lineBreak;
     }
 
     public int getOffsetLength() {
         return offsetLength;
     }
 
-    public void setOffsetLength(final int offsetLength) {
-        this.offsetLength = offsetLength;
-    }
 }
